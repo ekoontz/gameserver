31b2b6a1f34c6d34e0348317610d98f38beb5c0a
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-15 07:25:49 -0700

    +org.clojure/tools.logging

--- project.clj
+++ project.clj
@@ -8,7 +8,9 @@
                  [compojure "1.1.3"]
                  [stencil "0.3.2"] ;; A fast, compliant implementation of Mustache in Clojure.
                  [clj-http "0.7.3"]
-                 [cheshire "5.2.0"]] ;; Clojure JSON and JSON SMILE (binary json format) encoding/decoding
+                 [org.clojure/tools.logging "0.2.6"]
+                 [cheshire "5.2.0"] ;; Clojure JSON and JSON SMILE (binary json format) encoding/decoding
+                 [log4j/log4j "1.2.17"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}}
   :plugins [[lein-ring "0.8.5"]]
