c501c89f12a7ac7d8ec6d058c8e611f8104e2326
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-14 18:54:56 -0700

    +clojure.string/trim

--- src/gameserver/server.clj
+++ src/gameserver/server.clj
@@ -1,16 +1,17 @@
 (ns gameserver.server
-  (:require [ring.adapter.jetty :as jetty]
-            [gameserver.app :as app])
+  (:require  [clojure.string :as string :refer [trim]]
+             [ring.adapter.jetty :as jetty]
+             [gameserver.app :as app])
   (:gen-class))
 
 (defn start [system]
-  (println "GS server start..")
+  (println "The Gameserver is starting..")
   (let [server (jetty/run-jetty (var app/site-handler)
                                 {:host (:host system) 
                                  :port (:port system) 
                                  :join? false})]
-    (println "GS started.")
-    (println (str "GS listening at http://" (:host system) ":" (:port system)))
+    (println (str "The Gameserver has started and is "
+                  "listening at " (str "http://" (trim (:host system)) ":" (trim (:port system)))))
     server))
 
 (defn stop [instance]
