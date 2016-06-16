c1c5251248b6c29f95f0e73fba7d5f88c38aae1f
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-16 10:28:25 -0700

    add minimal and non-intrusive (current form-based login and registration works)

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [friend-oauth2 "0.1.3"]
                  [ring "1.2.0"]
                  [compojure "1.1.3"]
                  [stencil "0.3.2"] ;; A fast, compliant implementation of Mustache in Clojure.
--- src/gameserver/app.clj
+++ src/gameserver/app.clj
@@ -4,8 +4,10 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [stencil.loader :as stencil]
+            [friend-oauth2.workflow :as oauth2]
             [gameserver.middleware.session :as session-manager]
-            [gameserver.middleware.context :as context-manager]))
+            [gameserver.middleware.context :as context-manager]
+            [ring.util.response :as resp]))
 
 ;;; Initialization
 ;; Add required code here (database, etc.)
@@ -23,7 +25,9 @@
 ;;; Load generic routes
 (require '[gameserver.view.profile :refer [profile-routes]]
          '[gameserver.view.settings :refer [settings-routes]]
-         '[gameserver.view.admin :refer [admin-routes]])
+         '[gameserver.view.admin :refer [admin-routes]]
+         '[friend-oauth2.workflow :as oauth2]
+         '[cemerick.friend :as friend])
 
 ;;; Load website routes
 ;; Add your routes here
@@ -39,6 +43,19 @@
               admin-routes
               (route/resources "/")
               (route/not-found "<h1>Page not found.</h1>"))
+
+      (friend/authenticate
+
+       {:allow-anon? true
+        :login-uri "/login"
+        :default-landing-uri "/"
+        :unauthorized-handler #(->
+                                "unauthorized"
+                                resp/response
+                                (resp/status 401))
+
+        :workflows []})
+
       (session-manager/wrap-session)
       (context-manager/wrap-context-root)
       (handler/site)))
