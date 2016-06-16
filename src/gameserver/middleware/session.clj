1a14fa250d0f6d9a811af703f0610ecb95491216
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-16 10:13:26 -0700

    more logging

--- src/gameserver/middleware/session.clj
+++ src/gameserver/middleware/session.clj
@@ -18,6 +18,8 @@
       (when-let [flash (get-in request [:session :app-flash])]
         (reset! *flash* flash))
       (let [response (handler request)]
+        (log/info (str "wrap-session: handler response: " response))
+        (log/info (str "wrap-session: *session*:" @*session*))
         (let [retval
               (-> response
                   (assoc-in [:session :app-session] @*session*)
