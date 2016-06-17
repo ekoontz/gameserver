31cc820d66eb464bf641079dd2b887c44e39903a
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-16 20:02:48 -0700

    postgres db support

--- src/gameserver/service/db.clj
+++ src/gameserver/service/db.clj
@@ -1,5 +1,8 @@
 (ns gameserver.service.db
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.string :as string]
+            [clojure.tools.logging :as log]
+            [environ.core :refer [env]]
+            [korma.db :refer [default-connection defdb postgres]]))
 
 ;; A simple in-memory database for testing purpose.
 (def database (atom {}))
@@ -21,3 +24,41 @@
   (when (and username
            (not (get-user username)))
     (swap! database assoc (:username user) user)))
+
+(defdb korma-db
+  (let [default "postgres://postgres@localhost:5432/gameserver"
+        database-url (cond
+                      (env :database-url)
+                      (env :database-url)
+
+                      true default
+                      true
+                      (do
+                        (log/error
+                         (str "DATABASE_URL not set in your environment: you must define it; e.g.: " default)
+                         (throw (Exception. (str "could not find database name in your database-url."))))))]
+    ;; this constructs the actual database connection which is used throughout the code base.
+    (postgres
+     ;; thanks to Jeroen van Dijk via http://stackoverflow.com/a/14625874
+     (let [[_ user password host port db]
+           (re-matches #"postgres://(?:([^:]+):?(.*)@)?([^:]+)(?::(\d+))?/(.+)"
+                       database-url)
+
+           redacted-database-url
+           (if (and password (not (empty? password)))
+             (string/replace database-url
+                             (str ":" password)
+                             ":******")
+             database-url)
+           ]
+       (if (nil? db)
+         (throw (Exception. (str "could not find database name in your database-url: '"
+                                 database-url "'"))))
+
+       (log/info (str "scanned DATABASE_URL:" redacted-database-url "; found:"
+                      "(user,host,db)=(" user "," host "," db ")"))
+             {:db db
+              :user user
+              :password password
+              :host host
+              :port (or port "5432")}))))
