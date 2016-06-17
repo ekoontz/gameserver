daabec04eec55ef7e59205491c190ca309f712ef
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-16 17:12:12 -0700

    adding postgres support

--- project.clj
+++ project.clj
@@ -1,17 +1,29 @@
 (defproject gameserver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Verbcoach Gameserver"
+  :url "http://game.verbcoach.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [friend-oauth2 "0.1.3"]
-                 [ring "1.2.0"]
-                 [compojure "1.1.3"]
-                 [stencil "0.3.2"] ;; A fast, compliant implementation of Mustache in Clojure.
-                 [clj-http "0.7.3"]
-                 [org.clojure/tools.logging "0.2.6"]
                  [cheshire "5.2.0"] ;; Clojure JSON and JSON SMILE (binary json format) encoding/decoding
-                 [log4j/log4j "1.2.17"]]
+                 [clj-http "0.7.3"]
+                 [com.cemerick/friend "0.2.1"]
+                 [compojure "1.1.3"]
+                 [environ "1.0.0"]
+                 [friend-oauth2 "0.1.3"]
+                 [http-kit "2.1.16"]
+                 [korma "0.4.1"]
+                 [log4j/log4j "1.2.17"]
+
+                 ;; TODO: org.clojure/data.json is only used by
+                 ;; gameserver.view.auth.google: consider using one or the other
+                 ;; (i.e. org.clojure/data.json or cheshire) if possible, to avoid dependency bloat.
+                 [org.clojure/data.json "0.2.5"] 
+
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.postgresql/postgresql "9.4.1208.jre7"]
+                 [ring "1.2.0"]
+                 [slingshot "0.12.2"]
+                 [stencil "0.3.2"]] ;; A fast, compliant implementation of Mustache in Clojure.
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}}
   :plugins [[cider/cider-nrepl "0.11.0"]
