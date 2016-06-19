c567bd18aa5027729499310afa35de4cb75e2f80
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-19 02:08:47 -0400

    temporarily remove auth-related functionality in order to get (friend) to work

--- src/gameserver/view/auth.clj
+++ src/gameserver/view/auth.clj
@@ -13,36 +13,6 @@
             [ring.util.response :as resp]
             [stencil.core :as stencil]))
 
-(defn validate-form-credentials [data]
-  (log/info (str "fun-credential-fn: input: "
-                 (dissoc data :password))) ;; remove sensitive password before logging.
-  (let [username (:username data)
-        user-data (db/get-user username)]
-    (if (or (nil? username) (nil? user-data))
-      (log/warn "validate-form-credentials: no such user.")
-      (do
-        (log/info (str "found user-data: " user-data))
-        (session/set-user! {:username username})
-        {:identity data :roles #{::user}}))))
-
-(def config
-  {:allow-anon? true
-   :unauthorized-handler #(->
-                           "unauthorized"
-                           resp/response
-                           (resp/status 401))
-
-   ;; this way we won't pre-empt the form-based authentication:
-   ;; /login-via-google as opposed to /login, which will always show the form-based authentication form.
-   :login-uri "/login"
-
-   :workflows [
-               (workflows/interactive-form)
-               (oauth2/workflow google-auth/auth-config)
-               ]
-   
-   :credential-fn validate-form-credentials})
-
 (defn- signup-page
   "Render the signup page."
   [request]
@@ -89,25 +59,6 @@
       (session/set-user! (select-keys user [:username :type]))
       user)))
 
-(defn- login
-  "Process user login with username/password.
-   if success : returns 'ok'
-   if error : returns a message to be displayed to the user"
-  [request]
-  (log/info (str "WTF GOT HERE!" request))
-  (let [username (-> request :params :username)
-        password (-> request :params :password)]
-    (do
-      (log/info (str "processing login attempt with username: " username))
-      (if-let [auth (auth request)]
-        (do
-          (log/info (str "user with username: " username " sucessfully authenticated."))
-          "ok"
-          )
-        (do
-          (log/warn (str "user with username: " username " failed to authenticate."))
-          "Sorry: failed to authenticate you.")))))
-
 (defn- reset-pass-page
   "Render the reset password page."
   [request]
@@ -146,8 +97,6 @@
   (POST "/signup" request (signup request))
   (GET "/login" request (login-page request))
   (GET "/login/form" request (login-page request))
-  (POST "/login" request (login request))
-  (POST "/login/form" request (login request))
   (GET "/reset-pass" request (reset-pass-page request))
   (POST "/reset-pass" request (reset-pass request))
   (GET "/check-session" request (check-session request))
