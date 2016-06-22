ce7e8d4a86157168ac178f36d3b30a1526383744
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-22 12:37:45 -0400

    add example database entry

--- src/gameserver/service/db.clj
+++ src/gameserver/service/db.clj
@@ -2,8 +2,10 @@
   (:require [clojure.tools.logging :as log]))
 
 ;; A simple in-memory database for testing purpose.
-(def database (atom {}))
-
+(def database (atom {"ekoontz" {:username "ekoontz"
+                                :email "ekoontz@hiro-tan.org"
+                                :type "admin"
+                                :password "password"}}))
 (defn get-user
   "Returns the user corresponding to the given username."
   [username]
@@ -13,7 +15,6 @@
       (log/info (str "found username:" username  " in database."))
       user-record)
     (log/warn (str "did not find username:" username  " in database."))))
-    
 
 (defn add-user
   "Add a new user to database."
