d3a8b38e494e75fbc274dc4fc4dfda2affa3dda4
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-24 08:54:24 -0400

    reorder dependencies

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [friend-oauth2 "0.1.3"]
+  :dependencies [[clj-http "0.7.3"]
+                 [cheshire "5.2.0"] ;; Clojure JSON and JSON SMILE (binary json format) encoding/decoding
                  [ring "1.2.0"]
                  [compojure "1.1.3"]
-                 [stencil "0.3.2"] ;; A fast, compliant implementation of Mustache in Clojure.
-                 [clj-http "0.7.3"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [cheshire "5.2.0"] ;; Clojure JSON and JSON SMILE (binary json format) encoding/decoding
-                 [log4j/log4j "1.2.17"]
+                 [friend-oauth2 "0.1.3"]
                  [korma "0.4.2"]
-                 [org.postgresql/postgresql "9.4.1208.jre7"]]
+                 [log4j/log4j "1.2.17"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.postgresql/postgresql "9.4.1208.jre7"]
+                 [stencil "0.3.2"]] ;; A fast, compliant implementation of Mustache in Clojure.
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}}
   :plugins [[cider/cider-nrepl "0.11.0"]
