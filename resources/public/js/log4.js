3dd8db4ff685599a18b0857d68001dcf350381ba
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-27 11:52:54 -0700

    show a Firenze map

---
+++ resources/public/js/log4.js
@@ -0,0 +1,23 @@
+// Rough attempt to imitate http://logging.apache.org/log4j
+// Eugene Koontz (ekoontz@hiro-tan.org)
+// Licensed under Apache Software License version 2.
+// 
+// TODO: maybe add FATAL or CRITICAL or something even worse than ERROR.
+var TRACE = 4;
+var DEBUG = 3;
+var INFO  = 2;
+var WARN  = 1;
+var ERROR  = 0;
+var logging = {
+    4 : "TRACE",
+    3 : "DEBUG",
+    2 : "INFO",
+    1 : "WARN",
+    0 : "ERROR"
+};
+
+function log(level,str) {
+    if (logging_level >= level) {
+	console.log(logging[level] + ":" + str);
+    }
+}
