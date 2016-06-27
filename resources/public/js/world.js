2f412bb0a889544ac38958b783ea96aa3e38b13b
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-27 13:41:13 -0700

    show a polygon

--- resources/public/js/world.js
+++ resources/public/js/world.js
@@ -1,13 +1,16 @@
 var logging_level = INFO;
 
-var Firenze = [43.775535,11.248898];
+var Firenze = [[43.775535,11.248898],
+	       [43.774537,11.249122],
+	       [43.775424,11.251013],
+	       [43.775264,11.251037]];
 
 var mapbox_api_key = "";
 
 function load_world() {
     log(INFO,"loading world..");
-    var current_lat = Firenze[0];
-    var current_long = Firenze[1];
+    var current_lat = Firenze[0][0];
+    var current_long = Firenze[0][1];
     var current_zoom = 17;
     var map = L.map('map', {
 	// http://leafletjs.com/reference.html#map-options
@@ -29,4 +32,8 @@ function load_world() {
 	id: tileSet,
 	k: mapbox_api_key
     }).addTo(map);
+
+    marker = L.marker([current_lat, current_long]).addTo(map);
+    L.polygon([Firenze[3],Firenze[1],Firenze[0],Firenze[2]]).addTo(map);
+    
 }
