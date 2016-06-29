b64ccbe5dae32b9dd877895ce98420e0b461b880
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-29 14:36:00 -0700

    improve formatting

--- resources/public/js/world.js
+++ resources/public/js/world.js
@@ -29,8 +29,7 @@ function load_world() {
 	k: mapbox_api_key
     }).addTo(map);
     
-    //    L.geoJson(sallustiano.features, {
-    L.geoJson(sal_and_castro.features, {
+    L.geoJson(sallustiano_e_castro_pretorio.features, {
 	// https://gist.github.com/onderaltintas/6649521
 	coordsToLatLng: function(coords) {
 	    x = coords[0];
