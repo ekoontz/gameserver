90b7acf614f3c9ecbbc62f1a05f76319d873a7ed
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-29 22:08:30 -0700

    refactor/cleanup

--- resources/public/js/world.js
+++ resources/public/js/world.js
@@ -1,10 +1,5 @@
 var logging_level = INFO;
 
-var Roma = [[41.9012917,12.5012515],
-	    [41.9013996,12.5011637],
-	    [41.9011458,12.5008891],
-	    [41.9013364,12.5010894]];
-
 var mapbox_api_key = "";
 
 function show_player_turf(map,player,style) {
@@ -27,6 +22,10 @@ function show_player_turf(map,player,style) {
 
 function load_world() {
     log(INFO,"loading world..");
+    var Roma = [[41.9012917,12.5012515],
+		[41.9013996,12.5011637],
+		[41.9011458,12.5008891],
+		[41.9013364,12.5010894]];
     var current_lat = Roma[0][0];
     var current_long = Roma[0][1];
     var current_zoom = 11;
@@ -46,29 +45,22 @@ function load_world() {
 	id: tileSet,
 	k: mapbox_api_key
     }).addTo(map);
-
-    var marker = L.marker([41.9210211676156,12.566591169536]).addTo(map);
     
-    var player1 = {
+    show_player_turf(map,0,{
 	"fillColor": "#f5f000",
 	"weight": 1,
 	"opacity": 0.65
-    };
-
-    var player2 = {
+    });
+    show_player_turf(map,1,{
 	"fillColor": "#007ac7",
 	"weight": 1,
 	"opacity": 0.55
-    };
-
-    var player3 = {
+    });
+    show_player_turf(map,2,{
 	"fillColor": "#ff2000",
 	"weight": 1,
 	"opacity": 0.45
-    };
-
-    show_player_turf(map,0,player1);
-    show_player_turf(map,1,player2);
-    show_player_turf(map,2,player3);
+    });
+    var marker = L.marker([41.9210211676156,12.566591169536]).addTo(map);
 }
 
