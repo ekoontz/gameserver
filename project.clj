56f52e6e63e1c338624abe271a86730ac60eea3b
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-01 11:15:38 -0700

    upgrade ring, compojure, and jetty to latest stable releases

--- project.clj
+++ project.clj
@@ -5,8 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "0.7.3"]
                  [cheshire "5.2.0"] ;; Clojure JSON and JSON SMILE (binary json format) encoding/decoding
-                 [ring "1.2.0"]
-                 [compojure "1.1.3"]
+                 [ring "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [org.eclipse.jetty/jetty-server "8.1.19.v20160209"]
+                 [compojure "1.5.1"]
                  [http-kit "2.1.16"]
                  [friend-oauth2 "0.1.3"]
                  [korma "0.4.2"]
@@ -18,6 +20,7 @@
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}}
   :plugins [[cider/cider-nrepl "0.11.0"]
-            [lein-ring "0.9.3"]]
+            [lein-ring "0.9.7"]
+            ]
   :ring {:handler gameserver.app/site-handler}
   :main gameserver)
