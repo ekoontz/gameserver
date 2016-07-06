a6d613651d3df704b5c512a5750a367de0b2e10b
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-06 15:13:09 -0700

    +touch.sh

---
+++ src/scripts/touch.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh # TODO portability
+while [ 1 ]; do
+    gfind . -newer src/gameserver/app.clj \
+	  -a \( -name "*.clj" -o \
+	        -name "*.mustache" \) \
+	  -a -type f  \
+	| xargs -I{} bash -c "echo {}; touch src/gameserver/app.clj";
+    sleep 1;
+done
