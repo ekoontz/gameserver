52325ca0225ae03e765d254678bef53180e1701b
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-06 10:03:49 -0700

    rename moustache to mustache

--- resources/public/js/world.js
+++ resources/public/js/world.js
@@ -64,9 +64,6 @@ function load_adjacencies(map) {
 		count++;
 	    }
 	    log(INFO,"loaded " + count + " adjacencies.");
- 	    $.get('/mst/infobox.moustache', function(template) {
-		$('#infobox').append(Mustache.render(template,{}));
-	    });
 	}});
 }
 
@@ -184,5 +181,23 @@ function getNewBearing(from_centroid,to_centroid) {
 
 function update_infobox(new_hood_osm) {
     log(INFO,"update_infobox: " + new_hood_osm);
+    var name = "Testaccio";
+    var owner = "Eugene Koontz";
+    var adjacencies = [{name:"Regola"},
+		       {name:"Sant'Angelo"},
+		       {name:"Pigna"}];
+
+    var tokens = [{name:"cane",level:0},
+		  {name:"ragazzo",level:1},
+		  {name:"trapassato",meta:"meta",level:2}];
+			
+    $.get('/mst/infobox.mustache', function(template) {
+	$('#infobox').html(Mustache.render(template,{
+	    name:name,
+	    owner:owner,
+	    adjacencies:adjacencies,
+	    tokens:tokens,
+	}));
+    });
 }
 
