a6d86e40ee96e59adb62449c5b165e412855f9fc
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-06 20:43:44 -0700

    implement highlight-polygon-on-mouseover

--- resources/public/js/world.js
+++ resources/public/js/world.js
@@ -198,9 +198,43 @@ function getNewBearing(from_centroid,to_centroid) {
 }
 
 function highlight_place(map,osm_id) {
-    log(INFO,"highlighting_place: " + osm_id);
+    log(DEBUG,"highlighting_place: " + osm_id);
+    if (typeof(osm2hood[osm_id].polygon) == "undefined") {
+	$.ajax({
+	    async: false,
+	    cache:false, // we use our own cache (osm2hood), so don't waste more client memory here.
+	    dataType: "json",
+	    url: "/world/hoods/" + osm_id,
+	    success: function(content) {
+		osm2hood[osm_id].polygon = content;
+		highlight_polygon(map,osm2hood[osm_id].polygon);
+	    }
+	});
+    } else {
+	highlight_polygon(map,osm2hood[osm_id].polygon);
+    }
 }
 
+function highlight_polygon(map,polygon) {
+    if (map.getSource('highlighted')) {
+	map.removeSource('highlighted');
+    }
+
+    if (map.getLayer('highlighted')) {
+	map.removeLayer('highlighted');
+    }
+    map.addSource('highlighted',
+		  new mapboxgl.GeoJSONSource({
+		      type: "geojson",
+		      data: polygon}));
+    map.addLayer({
+	type: "fill",
+	paint: highlighted_layer_style,
+	id: "highlighted",
+	source: 'highlighted',
+	"source-layer": "highlighted"
+    });
+}
 
 function show_open_turf(map) {
     $.ajax({
@@ -221,5 +255,3 @@ function show_open_turf(map) {
 	    });
 	}});
 }
-
-
