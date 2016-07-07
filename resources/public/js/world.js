0cc4ecb1ea926581ad6eb34bc94ea9b15301489d
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-07 09:54:47 -0700

    move definitions of onclick() and onmove() to their own file

--- resources/public/js/world.js
+++ resources/public/js/world.js
@@ -115,65 +115,11 @@ function load_centroids(map) {
 		};
 	    }
 	    map.on('click',function(e) {
-		var pos = e.lngLat;
-		var features = map.queryRenderedFeatures(e.point);
-		if (features.length > 0) {
-		    for (var i = 0; i < features.length; i++) {
-			if (features[i].properties.admin_level == '10') {
-			    var old_hood = $("#player" + player_id + "-position").html();
-			    var new_hood = features[i].properties.neighborhood;
-			    log(DEBUG,"user clicked on place:" + new_hood + " with osm_id=" +
-				features[i].properties.osm_id);
-			    update_infobox(features[i].properties.osm_id);
-			    
-			    if (old_hood != new_hood) {
-				log(INFO,"selected hood:" + new_hood + " with pos:" + pos);
-				
-				var oldCentroid = centroids[old_hood];
-				var newCentroid = centroids[new_hood];
-
-				$("#player" + player_id + "-position").html(new_hood);
-				$("#player" + player_id + "-selected").html("");
-		    
-				var newBearing;
-				if (updateBearing == true) {
-				    newBearing = getNewBearing(oldCentroid,newCentroid);
-				} else {
-				    newBearing = map.getBearing();
-				}
-
-				map.flyTo({center: newCentroid,
-					   bearing: newBearing});
-		    
-				// break out of for() loop for efficiency, since
-				// we don't need to look at other features in the array - we
-				// only care about the neighborhood (admin_level == 10).
-			    }
-			    break;
-			}
-		    }
-		} else {
-		    log(DEBUG,"you didn't click on anything of importance at pos:" + pos);
-		}
+		onclick(e,map);
 	    }, false);
 
 	    map.on('mousemove',function(e) {
-		// display the selected hood in the player-selected box.
-		var features = map.queryRenderedFeatures(e.point);
-		if (features.length > 0) {
-		    for (var i = 0; i < features.length; i++) {
-			if (features[i].properties.admin_level == '10') {
-			    if ($("#player"+player_id+"-position").html() != features[i].properties.neighborhood) {
-				$("#player"+player_id+"-selected").html(features[i].properties.neighborhood);
-			    } else {
-				$("#player"+player_id+"-selected").html("");
-			    }
-			    var osm_id = features[i].properties.osm_id;
-			    highlight_place(map,osm_id);
-			    break;
-			}
-		    }
-		}
+		onmousemove(e,map);
 	    }, false);
 	}
     });
