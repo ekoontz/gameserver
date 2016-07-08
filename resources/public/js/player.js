b0a6b7de405c83400bce180789b4ee915d5ab9c3
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-08 10:39:45 -0700

    consolidate load_players() and update_players() into update_players()

--- resources/public/js/player.js
+++ resources/public/js/player.js
@@ -1,34 +1,3 @@
-function create_player_marker(map,player) {
-    // 1. show neighborhood of player:
-    $("#player"+player+"-position").html(players[player].location.properties.neighborhood);
-    // 2. show name of player:
-    $("#player"+player+"-name").html(players[player].name);
-
-    // 3. show marker of player in neighborhood:
-    map.addSource('player_marker'+player,
-		  new mapboxgl.GeoJSONSource({
-		      type: "geojson",
-		      data: players[player].location
-		  }));
-
-    // TODO: let players choose their icon.
-    icon = icons[player % icons.length];
-    
-    map.addLayer({
-        id: "player_marker"+player,
-        type: "symbol",
-        layout: {
-            "icon-image": icon,
-            "icon-offset":[0,-15],
-            "text-field":players[player].name,
-            "text-offset":[0,-1.5],
-	    "text-size":12,
-            "icon-size": 3
-        },
-        source: 'player_marker'+player
-    });
-}
-
 function update_player_marker(map,player) {
     // 1. show neighborhood of player:
     $("#player"+player+"-position").html(players[player].location.properties.neighborhood);
@@ -37,7 +6,42 @@ function update_player_marker(map,player) {
 
     // 3. show marker of player in neighborhood:
     var source = map.getSource('player_marker'+player);
-    source.setData(players[player].location);
+    if (typeof(source) == "undefined") {
+	// if source is undefined, this is the
+	// first call of update_player_marker().
+	map.addSource('player_marker'+player,
+		      new mapboxgl.GeoJSONSource({
+			  type: "geojson",
+			  data: players[player].location
+		      }));
+	source = map.getSource('player_marker'+player);
+    }
+
+    var layer = map.getLayer('player_marker'+player);
+    if (typeof(layer) == "undefined") {
+	// if layer is undefined, this is the
+	// first call of update_player_marker().
+
+	// TODO: let players choose their icon.
+	icon = icons[player % icons.length];
+	map.addLayer({
+            id: "player_marker"+player,
+            type: "symbol",
+            layout: {
+		"icon-image": icon,
+		"icon-offset":[0,-15],
+		"text-field":players[player].name,
+		"text-offset":[0,-1.5],
+		"text-size":12,
+		"icon-size": 3
+            },
+            source: 'player_marker'+player
+	});
+	layer = map.getLayer('player_marker'+player);
+    } else {
+	// not the first call.
+	source.setData(players[player].location);
+    }
 }
 
 function show_player_turf(map,player,css_class) {
@@ -72,7 +76,7 @@ function update_player_turf(map,player,css_class) {
     });
 }
 
-function load_players(map) {
+function update_players(map) {
     $.ajax({
 	async:true,
 	cache:true,
@@ -83,56 +87,23 @@ function load_players(map) {
 	    players = {};
 	    for (var i = 0; i < content.features.length; i++) {
 		var css_class = "player"+i;
-		var id = content.features[i].properties.player_id;
-		var player_record = { name: content.features[i].properties.player,
-				      id: id,
-				      location: content.features[i],
-				      css_class: css_class,
-				    }; 
-		players[id] = player_record;
-		create_player_marker(map,id);
-		show_player_turf(map,id,css_class);
-	    }
- 	    $.get('/mst/playerbox.mustache', function(template) {
-		$.each(players, function(key,value) {
-		    $('#playerbox').append(Mustache.render(template,value));
-		});
-	    });
-	}
-    });
-}
-
-function update_players(map) {
-    $.ajax({
-	async:true,
-	cache:true,
-	dataType: "json",
-	url: "/world/players",
-	success: function(content) {
-	    // refresh client-side 'players' db
-	    // TODO: don't just overwrite it - only
-	    // change things that have changed.
-	    players = {};
-	    for (var i = 0; i < content.features.length; i++) {
-		var css_class = "player"+i;
 		var player_id = content.features[i].properties.player_id;
-		var player_record = { name: content.features[i].properties.player,
-				      id: player_id,
-				      location: content.features[i],
-				      css_class: css_class,
-				    }; 
+		var player_record = {
+		    name: content.features[i].properties.player,
+		    id: player_id,
+		    location: content.features[i],
+		    css_class: css_class,
+		}; 
 		players[player_id] = player_record;
 		update_player_marker(map,player_id);
-		update_player_turf(map,player_id,css_class);
+//		update_player_turf(map,player_id,css_class);
 	    }
-
  	    $.get('/mst/playerbox.mustache', function(template) {
 		$.each(players, function(key,value) {
 		    $("#player"+value.id+"_box").remove();
 		    $('#playerbox').append(Mustache.render(template,value));
 		});
 	    });
-	    log(INFO,"Finished updating players' location and turf.");
 	}
     });
 }
