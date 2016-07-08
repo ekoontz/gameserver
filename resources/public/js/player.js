e16e0f877d2697b0c4bedbc63257a277f9ea3002
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-07 17:23:18 -0700

    update player info every 5 seconds

--- resources/public/js/player.js
+++ resources/public/js/player.js
@@ -26,6 +26,17 @@ function create_player_marker(map,player) {
     });
 }
 
+function update_player_marker(map,player) {
+    // 1. show neighborhood of player:
+    $("#player"+player+"-position").html(players[player].location.properties.neighborhood);
+    // 2. show name of player:
+    $("#player"+player+"-name").html(players[player].name);
+
+    // 3. show marker of player in neighborhood:
+    var source = map.getSource('player_marker'+player);
+    source.setData(players[player].location);
+}
+
 function show_player_turf(map,player,css_class) {
     $.ajax({
 	cache:false,
@@ -46,6 +57,17 @@ function show_player_turf(map,player,css_class) {
 	    });
 	}});
 };
+
+function update_player_turf(map,player,css_class) {
+    $.ajax({
+	cache:false,
+	dataType: "json",
+	url: "/world/player/"+player,
+	success: function(content) {
+	    map.getSource('player'+player).setData(content);
+	}
+    });
+}
 
 function load_players(map) {
     $.ajax({
@@ -73,6 +95,41 @@ function load_players(map) {
 		    $('#playerbox').append(Mustache.render(template,value));
 		});
 	    });
+	}
+    });
+}
+
+function update_players(map) {
+    $.ajax({
+	async:true,
+	cache:true,
+	dataType: "json",
+	url: "/world/players",
+	success: function(content) {
+	    // refresh client-side 'players' db
+	    // TODO: don't just overwrite it - only
+	    // change things that have changed.
+	    players = {};
+	    for (var i = 0; i < content.features.length; i++) {
+		var css_class = "player"+i;
+		var player_id = content.features[i].properties.player_id;
+		var player_record = { name: content.features[i].properties.player,
+				      id: player_id,
+				      location: content.features[i],
+				      css_class: css_class,
+				    }; 
+		players[player_id] = player_record;
+		update_player_marker(map,player_id);
+		update_player_turf(map,player_id,css_class);
+	    }
+
+ 	    $.get('/mst/playerbox.mustache', function(template) {
+		$.each(players, function(key,value) {
+		    $("#player"+value.id+"_box").remove();
+		    $('#playerbox').append(Mustache.render(template,value));
+		});
+	    });
+	    log(INFO,"Finished updating players' location and turf.");
 	}
     });
 }
