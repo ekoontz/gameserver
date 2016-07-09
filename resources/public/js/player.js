9445d5c2cb68d9349686462a98c92049a79bc736
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-09 09:56:27 -0700

    move update_players_from() from geolib to player.js

--- resources/public/js/player.js
+++ resources/public/js/player.js
@@ -48,3 +48,36 @@ function update_players(map) {
 	}
     });
 }
+
+function update_players_from(map,content) {
+    // populate client-side 'players' db
+    players = {};
+    for (var i = 0; i < content.features.length; i++) {
+	var css_class = "player"+i;
+	var player_id = content.features[i].properties.player_id;
+	var player_record = {
+	    name: content.features[i].properties.player,
+	    id: player_id,
+	    location: content.features[i],
+	    css_class: css_class,
+	    icon: icons[player_id % icons.length],
+	    points: 1, // TODO
+	    // how many places each player controls.
+	    places_count: content.features[i].properties.places_count
+	}; 
+	players[player_id] = player_record;
+	update_player_marker(map,player_id);
+	update_player_turf(map,player_id,css_class);
+    }
+    $.get('/mst/playerbox.mustache', function(template) {
+	$.each(players, function(key,player_record) {
+	    $("#player"+player_record.id+"_box").remove();
+	    $('#playerbox').append(Mustache.render(template,player_record));
+	    // add "onclick" for each playerbox:
+	    $("#player"+player_record.id+"_box").click(
+		function() {
+		    map.flyTo({center: player_record.location.geometry.coordinates});
+		});
+	});
+    });
+}
