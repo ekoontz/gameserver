c6f1fda6a11e15ecd59e90c532a65483a9c18990
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-08 20:37:17 -0700

    don't touch app.clj on .mustache files

--- src/scripts/touch.sh
+++ src/scripts/touch.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh # TODO portability
 while [ 1 ]; do
     gfind . -newer src/gameserver/app.clj \
-	  -a \( -name "*.clj" -o \
-	        -name "*.mustache" \) \
+	  -a \( -name "*.clj" \) \
 	  -a -type f  \
 	| xargs -I{} bash -c "echo {}; touch src/gameserver/app.clj";
     sleep 1;
