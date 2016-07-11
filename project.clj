78efa75768e82f8a1f96eca6e3fc6fda4499ccd7
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-11 13:15:59 -0700

    rewrite project.clj to fix dependencies

--- project.clj
+++ project.clj
@@ -3,25 +3,28 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[babel "1.6.1"]
-                 [clj-http "0.7.3"]
-                 [cheshire "5.2.0"] ;; Clojure JSON and JSON SMILE (binary json format) encoding/decoding
-                 [ring "1.5.0"]
-                 [ring/ring-jetty-adapter "1.5.0"]
-                 [org.eclipse.jetty/jetty-server "8.1.19.v20160209"]
-                 [compojure "1.5.1"]
-                 [http-kit "2.1.16"]
+  :dependencies [[babel "1.6.2"]
+                 [clj-http "2.2.0"]
+                 [dag_unify "1.2.4"]
                  [friend-oauth2 "0.1.3"]
-                 [korma "0.4.2"]
-                 [log4j/log4j "1.2.17"]
+                 [http-kit "2.1.16"]
+                 [log4j/log4j "1.2.16"]
                  [org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.cache "0.6.5"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.postgresql/postgresql "9.4.1208.jre7"]
-                 [stencil "0.3.2"]] ;; A fast, compliant implementation of Mustache in Clojure.
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
-                   :source-paths ["dev"]}}
+                 [org.clojure/tools.nrepl "0.2.7"]
+                 [stencil "0.5.0"]]
+
   :plugins [[cider/cider-nrepl "0.11.0"]
+            [lein-environ "1.0.0"]
             [lein-ring "0.9.7"]
-            ]
-  :ring {:handler gameserver.app/site-handler}
-  :main gameserver)
+            [s3-wagon-private "1.2.0"]]
+
+  :repositories {"s3" {:url "s3p://ekoontz-repo/releases/"
+                       :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY
+                       :passphrase :env/aws_secret_key ;; gets environment variable AWS_SECRET_KEY
+                       }}
+  :resource-paths ["resources"]
+  :ring {:handler gameserver.app/site-handler})
+
