58c3a2c02e0c1cda714964d2a6a3e544e81a5896
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-12 13:27:42 -0700

    improve appearance of input dialog with more context-sensitive cues

--- resources/public/js/ccd.js
+++ resources/public/js/ccd.js
@@ -2,7 +2,7 @@ function update_ccd(osm,current_player_id) {
     var info = osm2info(osm);
     info.top_message = "Capture it!";
     if (current_player_id == info.owner_id) {
-	info.top_message = "Welcome home!";
+	info.top_message = "Defend it!";
     }
     if (!info.owner_id) {
 	info.top_message = "Claim it!";
--- resources/public/js/input.js
+++ resources/public/js/input.js
@@ -2,38 +2,56 @@ function userinput_initialize() {
     $("#userinput").keyup(respond_to_user_input);
     $("#userinput").val("");
     $("#userinput").focus();
+    $('#response').html("<p class='input-spinner'>Please type something..<span class='fa fa-hand-o-up'> </span></p>");
 }
 
 function respond_to_user_input(event) {
     key_pressed = event.which;
     if (key_pressed != 13) {
-	// send to language server:
+	// send intermediate to language server:
 	var user_input = $("#userinput").val().trim();
+	if (user_input == "") {
+            $('#response').html("<p class='input-spinner'>Please type something..<span class='fa fa-hand-o-up'> </span></p>");
+	    return;
+	}
+
+        $('#response').html("<p class='input-spinner'><span class='fa fa-spinner fa-spin'> </span></p>");
 	$.ajax({
 	    cache: true,
 	    type: "GET",
             url: "/world/say/"+user_input}).done(function(content) {
-		log(INFO,"server responded; using response.");
+		var user_input_now = $("#userinput").val().trim();
+		if (user_input_now != user_input) {
+		    log(WARN,"user input has changed: ignoring this response.");
+		    return;
+		}
+		log(INFO,"server responded; using contents to populate #response.");
 		var vocab = [];
 		if (content.vocab.length > 0) {
 		    vocab = jQuery.map(content.vocab, function(word) {return {"word": word}});
 		}
-		var tenses = [];
+		var tenses;
 		if (content.tenses.length > 0) {
 		    tenses = jQuery.map(content.tenses, function(tense) {return {"tense": tense}});
+		}
+
+		var message = [];
+		if ((content.tenses.length == 0) && (content.vocab.length == 0)) {
+		    message = [{value: " "}];
 		}
 		
 		$.get('/mst/response.mustache', function(template) {
 		    $('#response').html(Mustache.render(template, {
 			vocab:vocab,
-			tenses:tenses
+			tenses:tenses,
+			message:message
 		    }));
 		});
 	    });
     }
 
     if (key_pressed == 13) {
-	// send to language server:
+	// user pressed return: send final results to language server:
 	$.ajax({
 	    cache: false,
 	    type: "POST",
