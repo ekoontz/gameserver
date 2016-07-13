4f27d972b4c010cbf3c590994db1243211500255
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-13 16:52:22 -0700

    showing vocab and tenses correctly coded for each solver

--- resources/public/js/placebox.js
+++ resources/public/js/placebox.js
@@ -8,17 +8,27 @@ function update_placebox(osm,current_player_id) {
 	info.top_message = "Claim it!";
     }
 
-    var random_style = ["","player0","player1","player2","player3"];
-    
-    info.vocab = jQuery.map(info.vocab_solved,function(item) {
-	return {name: item,
-		class: random_style[Math.floor(Math.random() * random_style.length)]}
-    });
+    info.vocab = [];
+    // first solved, then unsolved, since vocab is on the left of the layout
+    for (var i = 0; i < info.vocab_solved.length; i++) {
+	info.vocab.push({name: info.vocab_solved[i],
+			 class: players[info.vocab_solvers[i]].css_class});
+    };
+    for (var i = 0; i < info.vocab_unsolved.length; i++) {
+	info.vocab.push({name: info.vocab_unsolved[i],
+			 class: "open"});
+    };
 
-    info.tenses = jQuery.map(info.tenses_solved,function(item) {
-	return {name: item,
-		class: random_style[Math.floor(Math.random() * random_style.length)]};
-    });
+    info.tenses = [];
+    // first unsolved, then solved, since tenses are on the right of the layout
+    for (var i = 0; i < info.tenses_unsolved.length; i++) {
+	info.tenses.push({name: info.tenses_unsolved[i],
+			  class: "open"});
+    };
+    for (var i = 0; i < info.tenses_solved.length; i++) {
+	info.tenses.push({name: info.tenses_solved[i],
+			  class: players[info.tense_solvers[i]].css_class});
+    };
 
     if (!(typeof(info.vocab_unsolved) == "undefined")) {
 	info.remaining_vocab = info.vocab_unsolved.length;
