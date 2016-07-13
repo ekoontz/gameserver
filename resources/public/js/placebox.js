308d712b932e7aeeb3038c07010edcf5552e48bb
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-13 00:50:59 -0700

    get rid of #consolidated div

--- resources/public/js/placebox.js
+++ resources/public/js/placebox.js
@@ -9,7 +9,7 @@ function update_placebox(osm,current_player_id) {
     }
     $.get('/mst/placebox.mustache', function(template) {
 	// TODO: replace with #consolidated with #placebox when ready.
-	$('#consolidated').html(Mustache.render(template, info));
+	$('#placebox').html(Mustache.render(template, info));
     });
 }
  
