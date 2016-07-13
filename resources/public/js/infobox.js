3f669d2e4a06ab0b92a0694c08c63bf071519ef2
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-13 09:11:49 -0700

    cleanup: remove dead code

--- resources/public/js/infobox.js
+++
@@ -1,8 +0,0 @@
-function update_infobox(osm) {
-    var info = osm2info(osm);
-    $.get('/mst/infobox.mustache', function(template) {
-	$('#infobox').html(Mustache.render(template, info));
-    });
-}
-
-
