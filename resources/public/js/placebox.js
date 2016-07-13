1371813216330267ca11988b3b765c505fa0b68a
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-13 01:04:37 -0700

    move fake data from .mustache to .js

--- resources/public/js/placebox.js
+++ resources/public/js/placebox.js
@@ -7,6 +7,18 @@ function update_placebox(osm,current_player_id) {
     if (!info.owner_id) {
 	info.top_message = "Claim it!";
     }
+
+    info.remaining_vocab = 21;
+    info.remaining_grammar = 42;
+
+    info.vocab = [{name: "mangiare",
+		   class: "player2"},
+		  {name: "parlare"}];
+
+    info.tenses = [{name: "imperfetto"},
+		   {name: "passato prossimo",
+		    class: "player0"}];
+    
     $.get('/mst/placebox.mustache', function(template) {
 	// TODO: replace with #consolidated with #placebox when ready.
 	$('#placebox').html(Mustache.render(template, info));
