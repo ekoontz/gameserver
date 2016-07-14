c1f190a655a94fd470bcaf95b39cd06f2527e0ee
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-13 17:17:55 -0700

    handle undefined variables better

--- resources/public/js/placebox.js
+++ resources/public/js/placebox.js
@@ -10,25 +10,33 @@ function update_placebox(osm,current_player_id) {
 
     info.vocab = [];
     // first solved, then unsolved, since vocab is on the left of the layout
-    for (var i = 0; i < info.vocab_solved.length; i++) {
-	info.vocab.push({name: info.vocab_solved[i],
-			 class: players[info.vocab_solvers[i]].css_class});
-    };
-    for (var i = 0; i < info.vocab_unsolved.length; i++) {
-	info.vocab.push({name: info.vocab_unsolved[i],
-			 class: "unsolved"});
-    };
+    if (!(typeof(info.vocab_solved) == "undefined")) {
+	for (var i = 0; i < info.vocab_solved.length; i++) {
+	    info.vocab.push({name: info.vocab_solved[i],
+			     class: players[info.vocab_solvers[i]].css_class});
+	};
+    }
+    if (!(typeof(info.vocab_unsolved) == "undefined")) {
+	for (var i = 0; i < info.vocab_unsolved.length; i++) {
+	    info.vocab.push({name: info.vocab_unsolved[i],
+			     class: "unsolved"});
+	};
+    }
 
     info.tenses = [];
     // first unsolved, then solved, since tenses are on the right of the layout
-    for (var i = 0; i < info.tenses_unsolved.length; i++) {
-	info.tenses.push({name: info.tenses_unsolved[i],
-			  class: "unsolved"});
-    };
-    for (var i = 0; i < info.tenses_solved.length; i++) {
-	info.tenses.push({name: info.tenses_solved[i],
-			  class: players[info.tense_solvers[i]].css_class});
-    };
+    if (!(typeof(info.tenses_unsolved) == "undefined")) {
+	for (var i = 0; i < info.tenses_unsolved.length; i++) {
+	    info.tenses.push({name: info.tenses_unsolved[i],
+			      class: "unsolved"});
+	};
+    }
+    if (!(typeof(info.tenses_solved) == "undefined")) {
+	for (var i = 0; i < info.tenses_solved.length; i++) {
+	    info.tenses.push({name: info.tenses_solved[i],
+			      class: players[info.tense_solvers[i]].css_class});
+	};
+    }
 
     if (!(typeof(info.vocab_unsolved) == "undefined")) {
 	info.remaining_vocab = info.vocab_unsolved.length;
