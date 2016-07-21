578f477e79ec33e2bdf07cc6536abd6359d4b0a0
Eugene Koontz <ekoontz@hiro-tan.org>
2016-07-20 17:04:20 -0700

    working on vocab

---
+++ resources/public/js/vocab.js
@@ -0,0 +1,15 @@
+function show_vocab() {
+    $('#vocab-contents').html("<p class='input-spinner'><span class='fa fa-spinner fa-spin'> </span></p>");
+
+    $.ajax({cache: true,
+	    type: "GET",
+            url: "/world/vocab"}).done(
+		function(content) {
+		    var foo = 42;
+		}
+	    );
+}
+
+    
+
+	    
