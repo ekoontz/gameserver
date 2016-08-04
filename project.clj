19c80df985000978b456acc6fa84fc4fce6903c0
Eugene Koontz <ekoontz@hiro-tan.org>
2016-08-03 18:11:45 -0700

    upgrade babel

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/ekoontz/gameserver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[babel "1.7.2"]
+  :dependencies [[babel "1.8.0"]
                  [clj-http "2.2.0"]
                  [friend-oauth2 "0.1.3"]
                  [http-kit "2.1.16"]
