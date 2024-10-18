aee1802092da73765969a9af5b84933a8bc84a45
Eugene Koontz <ekoontz@hiro-tan.org>
2024-10-19 00:49:20 +0200

    able to 'lein ring server'

--- project.clj
+++ project.clj
@@ -8,13 +8,13 @@
                  [clj-http "2.2.0"]
                  [friend-oauth2 "0.1.3"]
                  [http-kit "2.1.16"]
-                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojure "1.11.3"]
                  [org.clojure/core.cache "0.6.5"]
                  [org.clojure/tools.logging "0.2.6"]
                  [stencil "0.5.0"]]
 
   :plugins [[lein-environ "1.0.0"]
-            [lein-ring "0.9.7"]
+            [lein-ring "0.12.5"]
             [s3-wagon-private "1.2.0"]]
 
   :repositories {"s3" {:url "s3p://ekoontz-repo/releases/"
