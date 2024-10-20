96eee9843e612925a16ee078e8f1e65f5415725d
Eugene Koontz <ekoontz@hiro-tan.org>
2024-10-20 12:09:24 +0200

    improve syntax consistency

--- src/gameserver/app.clj
+++ src/gameserver/app.clj
@@ -5,27 +5,24 @@
             [compojure.route :as route]
             [stencil.loader :as stencil]
             [gameserver.middleware.session :as session-manager]
-            [gameserver.middleware.context :as context-manager]))
+            [gameserver.middleware.context :as context-manager]
+
+            [gameserver.view.home :refer [home-routes]]
+            [gameserver.view.about :refer [about-routes]]
+            [gameserver.view.world :refer [world-routes]]
+
+            [gameserver.view.auth :refer [auth-routes authenticate]]
+
+            [gameserver.view.profile :refer [profile-routes]]
+            [gameserver.view.settings :refer [settings-routes]]
+            [gameserver.view.admin :refer [admin-routes]]
+            [friend-oauth2.workflow :as oauth2]
+            [cemerick.friend :as friend]))
 
 ;;; Initialization
 ;; Add required code here (database, etc.)
 (stencil/set-cache (cache/ttl-cache-factory {}))
 ;;(stencil/set-cache (cache/lru-cache-factory {}))
-
-;;; Load public routes
-(require '[gameserver.view.home :refer [home-routes]]
-         '[gameserver.view.about :refer [about-routes]]
-         '[gameserver.view.world :refer [world-routes]])
-
-;;; Load registration and authentication routes
-(require '[gameserver.view.auth :refer [auth-routes authenticate]])
-
-;;; Load generic routes
-(require '[gameserver.view.profile :refer [profile-routes]]
-         '[gameserver.view.settings :refer [settings-routes]]
-         '[gameserver.view.admin :refer [admin-routes]]
-         '[friend-oauth2.workflow :as oauth2]
-         '[cemerick.friend :as friend])
 
 ;;; Load website routes
 ;; Add your routes here
